4de68a081e690e3316b0d673d3922e6048dc6938
Twidt <t.c.twidt@gmail.com>
2018-01-17 23:49:47 -0500

    first attempt

---
+++ logic.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	// initializing some firebase
+	var config = {
+    apiKey: "AIzaSyBUi220bVJBSjLVDSo6nAfaIuUj1gtW90w",
+    authDomain: "train-2fce2.firebaseapp.com",
+    databaseURL: "https://train-2fce2.firebaseio.com",
+    projectId: "train-2fce2",
+    storageBucket: "",
+    messagingSenderId: "765138792762"
+  };
+firebase.initializeApp(config);
+})
